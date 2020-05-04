d994580334fd5deb89f7f4055ba29972a7d97a0d
Jordi Sabadell <jordisabadell@gmail.com>
2020-05-04 18:12:59 +0200

    Add arguments and order by priority funciont

--- functions.py
+++ functions.py
@@ -1,51 +1,27 @@
 import os
 import codecs
 
-def loadFile(fileName):
+def loadFile(fileName, ignoreHeaderRow):
 
     #read file
     file = open(fileName, "r", encoding="utf8")
 
-    #get header from first line
-    header = file.readline().replace("\n", "").split("\t")
+    if ignoreHeaderRow:
+        file.readline()
 
     #iterate lines
     items = []
     for i, line in enumerate(file):
         if i>0:
             if line:
-                #convert line to dictionary
-                item = {}
+                #convert line to array of elements
+                item = []
                 values = line.replace("\n", "").split("\t")
-                for j, value in enumerate(values):
-                    item[header[j].strip()] = value.strip()
-                
+                for value in values:
+                    item.append(value.strip())
+
                 items.append(item)
     
-    return header, items
-
-def sortItemsByParent(parentRootId, childColumnName, parentColumnName, items, depth):
-
-    result = []
-
-    print("Analysing "+ childColumnName +" = "+ str(parentRootId) +" / depth="+ str(depth) +"...")
-    for item in items:
-        if int(item[parentColumnName]) == int(parentRootId):
-            
-            print(parentColumnName +" = "+ str(item[childColumnName]) +"...")            
-            
-            #append parent to result
-            result.append(item)
-
-            #explore childs
-            childs = sortItemsByParent(item[childColumnName], childColumnName, parentColumnName, items, depth+1)
-            if childs:
-                for child in childs:
-                    result.append(child)
-
-    return result
-
-def sortItemsByPriority(priorityColumnName, items):
     return items
 
 def saveFile(fileName, items):
@@ -56,7 +32,59 @@ def saveFile(fileName, items):
 
     #wirte output file
     f = codecs.open(fileName, "a", "utf-8")
-    f.write('\n'.join(map(str, items)))
+    for item in items:
+        f.write('\t'.join(map(str, item))+"\n")
     f.close()
 
     return True
+
+def sortItemsByPriority(items, parentColumn, priorityColumn):
+    
+    #add temporal column with id+priority, order by this and remove this
+    for item in items:
+        item.append(int(item[parentColumn]) + int(item[priorityColumn]))
+
+    tmpCol = len(items[0])-1
+
+    #sort by temporal col
+    for i, item in enumerate(items):
+        x = int(item[tmpCol])
+        j = i 
+        while j>0 and int(items[j-1][tmpCol]) > x:
+            items[j] = items[j-1]
+            j = j - 1
+        items[j] = item
+    
+    #remove temporal col
+    for i, item in enumerate(items):
+        items[i] = item[0:tmpCol]
+
+    return items
+
+def sortItemsByParent(parentRootId, childColumn, parentColumn, 
+    addDepthCol, items, depth):
+
+    depth = depth + 1
+    result = []
+
+    for item in items:
+        if int(item[parentColumn]) == int(parentRootId):
+
+            #append parent to result
+            if addDepthCol:
+                item.append(depth)
+            result.append(item)
+
+            #explore childs
+            childs = sortItemsByParent(item[childColumn], childColumn, parentColumn,
+                addDepthCol, items, depth)
+            
+            if childs:
+                for child in childs:
+                    result.append(child)
+    
+    return result
+
+def getHeaderRow(fileName):
+    file = open(fileName, "r", encoding="utf8")
+    return file.readline().replace("\n", "").split("\t")
--- main.py
+++ main.py
@@ -1,39 +1,55 @@
 import argparse
-from functions import loadFile, saveFile, sortItemsByParent, sortItemsByPriority
+from functions import loadFile, saveFile, sortItemsByPriority, sortItemsByParent, getHeaderRow
 
 #ARGUMENTS
-#--inputfile pages.csv --childcol layoutId --parentcol parentLayoutId --prioritycol priority --newcoltreeformat True
+#example: py main.py --inputfile c:/tmp/pages.csv --childcol 1 --parentcol 2 --prioritycol 6 --ignoreheaderrow --adddepthcol
 parser = argparse.ArgumentParser(description='Given a CSV file with parent-child relationship column, this task sort the rows. Optional it creates a new column with tree format.')
-parser.add_argument('--inputfile', dest='inputFileName', help='Input file name.', type=str, required=True)
-parser.add_argument('--childcol', dest='childColumnName', help='Child column name.', type=str, required=True)
-parser.add_argument('--parentcol', dest='parentColumnName', help='Parent column name.', type=str, required=True)
-parser.add_argument('--parentrootid', dest='parentRootId', help='Parent root ID.', type=int, required=False, default=0)
-parser.add_argument('--prioritycol', dest='priorityColumnName', help='Priority column name.', type=str, required=False, default='')
-parser.add_argument('--newcoltreeformat', dest='newColumnTreeFormat', help='New column with tree format.', type=bool, required=False, default=False)
-parser.add_argument('--outputfile', dest='outputFileName', help='Output file name.', type=str, required=False, default='')
+parser.add_argument('--inputfile', dest='input_file', 
+    help='Input file name.', type=str, required=True)
+parser.add_argument('--outputfile', dest='output_file', 
+    help='Output file name.', type=str, required=False, default='')
+parser.add_argument('--childcol', dest='child_column', 
+    help='Child column number (default value 0: first column).', type=int, required=True, default=0)
+parser.add_argument('--parentcol', dest='parent_column', default=1, 
+    help='Parent column number (default value 1: second column).', type=int, required=True)
+parser.add_argument('--prioritycol', dest='priority_column', 
+    help='Priority column number.', type=int, required=False)
+parser.add_argument('--parentrootid', dest='parent_root_id', 
+    help='Parent root ID (default value 0).', type=int, required=False, default=0)
+parser.add_argument('--ignoreheaderrow', 
+    help='Ignore first row (header).', action="store_true")
+parser.add_argument('--adddepthcol', 
+    help='Add new column with tree depth.', action="store_true")
 
-# PARAMS
 args = parser.parse_args()
-inputFileName = args.inputFileName
-childColumnName = args.childColumnName
-parentColumnName = args.parentColumnName
-parentRootId = args.parentRootId
-priorityColumnName = args.priorityColumnName
-newColumnTreeFormat = args.newColumnTreeFormat
-outputFileName = args.outputFileName
+inputFile = args.input_file
+outputFile = args.output_file
+childColumn = args.child_column
+parentColumn = args.parent_column
+priorityColumn = args.priority_column
+parentRootId = args.parent_root_id
+ignoreHeaderRow = args.ignoreheaderrow
+addDepthCol = args.adddepthcol
 
-if not outputFileName:
-    outputFileName = inputFileName.replace(".csv", "_sorted.csv")
-
-print("inputFileName=" + inputFileName + ", childColumnName=" + childColumnName + 
-    ", parentColumnName=" + parentColumnName + ", parentRootId=" + str(parentRootId) + 
-    ", priorityColumnName=" + priorityColumnName + ", newColumnTreeFormat=" + str(newColumnTreeFormat) + 
-    ", outputFileName=" + outputFileName)
+if not outputFile:
+    outputFile = inputFile.replace(".csv", "_sorted.csv")
 
 #EXECUTE
-header, items = loadFile(inputFileName)
-itemsSorted = sortItemsByParent(parentRootId, childColumnName, parentColumnName, items, 0)
-if priorityColumnName:
-    itemsSorted = sortItemsByPriority(priorityColumnName, itemsSorted)
+items = loadFile(inputFile, ignoreHeaderRow)
 
-saveFile(outputFileName, itemsSorted)
+if priorityColumn:   
+    items = sortItemsByPriority(items, parentColumn, priorityColumn)
+
+itemsSorted = sortItemsByParent(parentRootId, childColumn, 
+    parentColumn, addDepthCol, items, -1)
+
+if ignoreHeaderRow:
+    #It means that exists first row as Header. Concatenate it.
+    header = getHeaderRow(inputFile)
+    if addDepthCol:
+        header.append("__auto_generated_depth_col__")
+    itemsSorted = [header] + itemsSorted
+
+saveFile(outputFile, itemsSorted)
+
+print("Done.")
